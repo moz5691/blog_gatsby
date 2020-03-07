4f62d138a08668599491975867019469d709222c
Chanho Ahn <moz5691@gmail.com>
2020-03-07 12:49:10 -0500

    .gitignore fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,6 +34,20 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [{
+          resolve: `gatsby-remark-katex`,
+          options: {
+            // Add any KaTeX options from
+            // https://github.com/KaTeX/KaTeX/blob/master/docs/options.md here
+            strict: `ignore`
+          }
+        }],
+      },
+    },
+
+    {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: process.env.GOOGLE_ANALYTICS_ID,
